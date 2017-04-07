ff81930cf28265a3934a49c0b1e85f5fa5a1163b
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-04-07 16:28:47 -0300

    MULE-12093: PagingProvider should support sticky connections

--- modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/PagedOperationExecutionTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/PagedOperationExecutionTestCase.java
@@ -8,6 +8,7 @@ package org.mule.runtime.module.extension.internal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.isIn;
 import static org.hamcrest.core.Is.is;
 import static org.mule.test.heisenberg.extension.MoneyLaunderingOperation.INVOLVED_PEOPLE;
@@ -71,6 +72,15 @@ public class PagedOperationExecutionTestCase extends ExtensionFunctionalTestCase
   public void pagedOperationUsingConnection() throws Exception {
     Iterator iterator = getCursor("pagedOperationUsingConnection");
     assertThat(iterator.next().toString(), containsString(SAUL_NEW_NUMBER));
+  }
+
+  @Test
+  public void pagedOperationWithStickyConnection() throws Exception {
+    Iterator<Integer> iterator = getCursor("pagedOperationWithStickyConnection");
+    Integer connectionId1 = iterator.next();
+    Integer connectionId2 = iterator.next();
+
+    assertThat(connectionId1, equalTo(connectionId2));
   }
 
   private <T> CursorIterator<T> getCursor(String flowName) throws Exception {
